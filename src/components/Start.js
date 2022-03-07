4b4ae1e2573e4d2a59b9d5d0e29b5848c6e68455
Attilio Pittelli <70145737+leo-ai-for-trading@users.noreply.github.com>
2022-03-07 17:09:05 +0100

    Update Start.js

--- src/components/Start.js
+++ src/components/Start.js
@@ -5,9 +5,9 @@ const Start = ({ onQuizStart }) => {
     <div className="card">
       <div className="card-content">
         <div className="content">
-          <h1>Inizia il quiz</h1>
-          <p>Buona fortuna!</p>
-          <button className="button is-info is-medium" onClick={onQuizStart}>Inizia</button>
+          <h1>Start the quiz</h1>
+          <p>Good luck!</p>
+          <button className="button is-info is-medium" onClick={onQuizStart}>Start</button>
         </div>
       </div>
     </div>
