469e3376c78cdda727e3608ea1f842733ef2f251
Attilio Pittelli <70145737+leo-ai-for-trading@users.noreply.github.com>
2022-03-07 17:18:15 +0100

    Update Modal.js

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -6,7 +6,7 @@ const Modal = ({ onClose, results, data }) => {
       <div className="modal-background" onClick={onClose}></div>
       <div className="modal-card">
         <header className="modal-card-head">
-          <p className="modal-card-title">Your answers</p>
+          <p className="modal-card-title">La tua risposta</p>
           <button className="delete" onClick={onClose}></button>
         </header>
         <section className="modal-card-body content">
@@ -14,8 +14,8 @@ const Modal = ({ onClose, results, data }) => {
             {results.map((result, i) => (
               <li key={i} className="mb-6">
                 <p><strong>{result.q}</strong></p>
-                <p className={result.a === data[i].answer ? 'has-background-success has-text-white p-2' : 'has-background-danger has-text-white p-2'}>Your answer: {result.a}</p>
-                {result.a !== data[i].answer && <p className="has-background-link has-text-white p-2">Correct answer: {data[i].answer}</p>}
+                <p className={result.a === data[i].answer ? 'has-background-success has-text-white p-2' : 'has-background-danger has-text-white p-2'}>La tua risposta: {result.a}</p>
+                {result.a !== data[i].answer && <p className="has-background-link has-text-white p-2">Risposta corretta: {data[i].answer}</p>}
               </li>
             ))}
           </ul>
